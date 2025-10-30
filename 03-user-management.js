fae3e2df4d0ea973b978a539354304d9cf5fe73e
cndd-k2 <akacandidek2@gmail.com>
2025-10-30 12:31:34 +0100

    feat: implement whoIsAdmin function to return admin names

--- 03-user-management.js
+++ 03-user-management.js
@@ -1,13 +1,53 @@
-// Crée une fonction whoIsAdmin qui affiche les noms des utilisateurs qui sont admin qui prends en paramètre un tableau d'objet d'utilisateurs .
+// Crée une fonction whoIsAdmin qui affiche les noms des utilisateurs qui sont admin qui prends 
+// en paramètre un tableau d'objet d'utilisateurs .
 // Chaque objet utilisateur a les propriétés suivantes :
 // - nom (string)
 // - age (number)
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
+function whoIsAdmin(users) {
+
+	let adminList = [];
+	for (let x in users) {
+		if ( users[x].estAdmin ) {
+			adminList.push(users[x].name)
+		}
+	}
+
+	return adminList
 }
+
+const myUsers = [
+		{
+			name: "Naruto Uzumaki",
+			age: 45,
+			estAdmin: false,
+		},
+		{
+			name: "Orochimaru",
+			age: 45,
+			estAdmin: false,
+		},
+		{
+			name: "Itachi Uchiha",
+			age: 75,
+			estAdmin: true,
+		},
+		{
+			name: "Kakashi Hatake",
+			age: 35,
+			estAdmin: true,
+		},
+		{
+			name: "Gaara",
+			age: 25,
+			estAdmin: false,
+		},
+	]
+
+
+console.log(whoIsAdmin(myUsers))
 
 module.exports = {
 	whoIsAdmin,
