1ef1a5b80c72289dd83964ea5b72910b634c2f93
cndd-k2 <akacandidek2@gmail.com>
2025-10-30 12:18:36 +0100

    : feat: implement averageNote

--- 02-array-average.js
+++ 02-array-average.js
@@ -4,8 +4,28 @@
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
 
 function averageNote(notes) {
-	
-}
+	let sum = 0;
+	for (let i = 0; i < notes.length; i++) {
+		sum += notes[i];
+	}
+	let averageNote = sum / notes.length;
+
+
+	if (averageNote >= 10) {
+		return {
+			moyenne: averageNote,
+			message: "Réussi",
+			notes: notes,
+		};
+	} else{
+		return "Échoué";
+	}
+
+} 
+
+console.log(averageNote([4, 8, 15, 16, 23]));
+
+
 
 module.exports = {
 	averageNote,
